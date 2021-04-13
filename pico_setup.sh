1ff656cc929f05015cfeed653ae2bb7f8976cc9e
Michael Stoops <spam@michaelstoops.com>
2021-04-12 20:14:03 -0700

    Redesigned the tests

--- pico_setup.sh
+++ pico_setup.sh
@@ -22,10 +22,16 @@
 
 # Exit on error
 set -e
-# Show all commands
-set -x
-# Trying to use an non-existent variable is an error
+
+# Trying to use a non-existent variable is an error
 set -u
+
+# if printenv DEBUG >& /dev/null; then
+    # Show all commands
+    set -x
+
+    env
+# fi
 
 # Number of cores when running make
 JNUM=4
@@ -37,7 +43,6 @@ else
     TARGET_DIR="$(pwd)/pico"
     echo "Using target dir: ${TARGET_DIR}"
 fi
-
 
 linux() {
     # Returns true iff this is running on Linux
@@ -78,14 +83,28 @@ raspberry_pi() {
     return 1
 }
 
+sudo_wrapper() {
+    # Some platforms have different needs for invoking sudo. This wrapper encapsulates that complexity.
+    # The output of this function should be a string on stdout, which will be used as a command. Example:
+    #   `$(sudo_wrapper) whoami`
+    # The above may equate to:
+    #   `sudo -i whoami`
+
+    if [ "${USER}" = root ]; then
+        # if we're already root, don't sudo at all. Relevant to some Docker images that don't have sudo but already run as root.
+        return
+    fi
+
+    # EC2 AMIs tend to have the user password unset, so you can't sudo without -i. It will cd /root, so you have to be
+    # careful with relative paths in the command.
+    echo sudo -i
+}
+
 phase_0() {
     # Preflight check
     # Checks the baseline dependencies. If you don't have these, this script won't work.
     echo "Entering phase 0: Preflight check"
     
-    echo "Verifying sudo access"
-    sudo -v
-
     if mac; then
         echo "Running on macOS"
         if which brew >> /dev/null; then
@@ -105,23 +124,49 @@ phase_0() {
 
         if which apt >> /dev/null; then
             echo "Found apt"
-            sudo apt update
+            $(sudo_wrapper) apt update
         else
-            echo 'This script requires apt, the default package manager for Debian and Debian-derived distros such as Ubuntu, and Raspberry Pi OS.'
+            echo 'This script requires apt, the default package manager for Debian and Debian-derived distros such as Ubuntu and Raspberry Pi OS.'
             echo "Stopping."
             exit 1
         fi
     fi
 }
 
+fail() {
+    # Outputs a failure message and exits with the error code output by the previous call.
+    # All args are echoed as a failure message.
+
+    R="${?}"
+    echo "Validation failed! :'-("
+    if [ ${*} ]; then
+        echo "${*}"
+    fi
+    exit ${R}
+}
+
+validate_git_repo() {
+    # tests that the given relative path exists and is a git repo
+    git -C ${TARGET_DIR}/${1} status >& /dev/null || fail
+}
+
+validate_toolchain_linux() {
+    # test that the expected packages are installed
+    dpkg-query -s git cmake gcc-arm-none-eabi build-essential gdb-multiarch automake autoconf build-essential texinfo libtool libftdi-dev libusb-1.0-0-dev >& /dev/null || fail
+}
+
 install_dev_env_deps_linux() {
     # Install development environment dependencies for Linux
 
-    DEPS="python3 git cmake gcc-arm-none-eabi build-essential gdb-multiarch automake autoconf build-essential texinfo libtool libftdi-dev libusb-1.0-0-dev minicom pkg-config"
+    # Avoid a certain dependency by installing ssh-client without recommends. See
+    # https://github.com/raspberrypi/pico-setup/pull/20#discussion_r608793993 for details.
+    $(sudo_wrapper) apt install -y --no-install-recommends ssh-client
+
+    DEPS="autoconf automake build-essential cmake gcc-arm-none-eabi gdb-multiarch git libftdi-dev libtool libusb-1.0-0-dev minicom pkg-config python3 texinfo"
     if debian || ubuntu; then
         DEPS="${DEPS} libstdc++-arm-none-eabi-newlib"
     fi
-    sudo apt install -y ${DEPS}
+    $(sudo_wrapper) apt install -y ${DEPS}
 }
 
 brew_install_idempotent() {
@@ -130,13 +175,18 @@ brew_install_idempotent() {
     return ${?}
 }
 
+validate_toolchain_mac() {
+    # test that the expected packages are installed
+    brew list git cmake pkg-config libtool automake libusb wget pkg-config gcc texinfo arm-none-eabi-gcc >& /dev/null 
+}
+
 install_dev_env_deps_mac() {
     # Install development environment dependencies for mac
 
-    brew_install_idempotent git cmake pkg-config libtool automake libusb wget pkg-config gcc texinfo minicom ArmMbed/homebrew-formulae/arm-none-eabi-gcc
+    brew_install_idempotent ArmMbed/homebrew-formulae/arm-none-eabi-gcc automake cmake git libtool libusb gcc minicom pkg-config texinfo wget
 }
 
-create_working_dir() {
+create_TARGET_DIR() {
     # Creates ./pico directory if necessary
 
     mkdir -p "${TARGET_DIR}"
@@ -156,35 +206,43 @@ clone_raspberrypi_repo() {
         BRANCH=master
     fi
 
-    # Save the working directory
-    pushd "${TARGET_DIR}" >> /dev/null
-
     REPO_URL="https://github.com/raspberrypi/${REPO_NAME}.git"
-    DEST="${TARGET_DIR}/${REPO_NAME}"
+    REPO_DIR="${TARGET_DIR}/${REPO_NAME}"
 
-    if [ -d "${DEST}" ]; then
-        echo "Not cloning ${DEST} because it already exists. If you really want to start over, delete it: rm -rf ${DEST}"
+    if [ -d "${REPO_DIR}" ]; then
+        echo "${REPO_DIR} already exists. Updating."
+        git -C "${REPO_DIR}" pull --ff-only
     else
         echo "Cloning ${REPO_URL}"
         if [ ${#} -gt 0 ]; then
-            git clone -b "${BRANCH}" "${REPO_URL}" ${*}
+            git -C "${TARGET_DIR}" clone -b "${BRANCH}" "${REPO_URL}" ${*}
         else
-            git clone -b "${BRANCH}" "${REPO_URL}"
+            git -C "${TARGET_DIR}" clone -b "${BRANCH}" "${REPO_URL}"
         fi
 
         # Any submodules
-        cd "${DEST}"
-        git submodule update --init
+        git -C "${REPO_DIR}" submodule update --init
     fi
+}
 
-    # Restore working directory
-    popd >> /dev/null
+warn_for_bashrc() {
+    # Earlier versions of this script set environment variables in .bashrc. The location has moved to .profile or
+    # .zprofile. If the user has a .bashrc defining any pico dev env variables, they could conflict with the settings
+    # in the other files. This function raises a warning for the user.
+
+    REGEX="^\s*export\s+\"?PICO_SDK_PATH="
+    if grep -q "${REGEX}" ~/.bashrc; then
+        echo "Your ~/.bashrc file contains the following line, which may conflict with this script's settings. We recommend removing it to prevent possible issues."
+        echo -n "    "; grep "${REGEX}" ~/.bashrc
+    fi
 }
 
 set_env() {
     # Permanently sets an environment variable by adding it to the current user's profile script
-    # $1 should be in the form of FOO=foo
-    EXPR="${1}"
+    # The form of the arguments should be `FOO foo`, which sets the environment variable `FOO=foo`
+    NAME="${1}"
+    VALUE="${2}"
+    EXPR="${NAME}=${VALUE}"
 
     # detect appropriate file for setting env vars
     if echo "${SHELL}" | grep -q zsh; then
@@ -205,12 +263,29 @@ set_env() {
 
     # set for now
     export "${EXPR}"
-    
+
     # set for later
-    if ! grep -q "^export ${EXPR}$" "${FILE}"; then
+    REGEX="^\s*export\s+\"?${NAME}="
+    if grep -q "${REGEX}" "${FILE}"; then
+        # Warn the user
+        echo "Your ${FILE} already contains the following environment variable definition(s):"
+        grep "${REGEX}" "${FILE}"
+        echo "This script would normally set the following line. We're adding it, but commented out, so that you can choose which you want."
+        echo "export \"${EXPR}\""
+        # Write to file
+        echo "# pico_setup.sh commented out the following line because it conflicts with another line in this file. You should choose one or the other." >> "${FILE}"
+        echo "# export \"${EXPR}\"" >> "${FILE}"
+    else
         echo "Setting env variable ${EXPR} in ${FILE}"
         echo "export \"${EXPR}\"" >> "${FILE}"
     fi
+}
+
+validate_pico_sdk() {
+    validate_git_repo pico-sdk
+
+    # test that the SDK env var is set and correct
+    test "${PICO_SDK_PATH}" = "${TARGET_DIR}/pico-sdk" || fail
 }
 
 setup_sdk() {
@@ -218,13 +293,21 @@ setup_sdk() {
     clone_raspberrypi_repo pico-sdk
 
     # Set env var PICO_SDK_PATH
-    set_env "PICO_SDK_PATH=${TARGET_DIR}/pico-sdk"
+    set_env PICO_SDK_PATH "${TARGET_DIR}/pico-sdk"
+}
+
+validate_uart() {
+    # test that the UART is configured. Only works on Raspberry Pi OS on Raspberry Pi hardware.
+    dpkg-query -s minicom  >& /dev/null || fail
+    grep -q "enable_uart=1" /boot/config.txt || fail
+    # note that the test for console=serial0 tests for the absence of a string
+    grep -q "console=serial0" /boot/cmdline.txt && fail
 }
 
 enable_uart() {
     # Enable UART
     echo "Disabling Linux serial console (UART) so we can use it for pico"
-    sudo raspi-config nonint do_serial 2
+    $(sudo_wrapper) raspi-config nonint do_serial 2
     echo "You must run sudo reboot to finish UART setup"
 }
 
@@ -234,17 +317,21 @@ phase_1() {
 
     if mac; then
         install_dev_env_deps_mac
+        validate_toolchain_mac
     else
         install_dev_env_deps_linux
+        validate_toolchain_linux
     fi
 
-    create_working_dir
+    create_TARGET_DIR
     setup_sdk
+    validate_pico_sdk
 
     if raspberry_pi && which raspi-config >> /dev/null; then
         enable_uart
+        validate_uart
     else
-        echo "Not configuring UART because this is not a Raspberry Pi computer, or raspi-config is unavailable."
+        echo "Not configuring UART, because either this is not a Raspberry Pi computer, or raspi-config is not available."
     fi
 }
 
@@ -252,7 +339,9 @@ build_examples() {
     # Build a couple of examples
     echo "Building selected examples"
     
-    cd "$TARGET_DIR/pico-examples"
+    # Save the working directory
+    pushd "${TARGET_DIR}/pico-examples" >> /dev/null
+
     mkdir -p build
     cd build
     cmake ../ -DCMAKE_BUILD_TYPE=Debug
@@ -263,6 +352,30 @@ build_examples() {
         make -j${JNUM}
         cd ..
     done
+
+    # Restore the working directory
+    popd >> /dev/null
+}
+
+validate_pico_extras() {
+    validate_git_repo pico-extras
+}
+
+validate_pico_examples() {
+    validate_git_repo pico-examples
+
+    # test that blink is built
+    test -f ${TARGET_DIR}/pico-examples/build/blink/blink.uf2 || fail
+
+    # test that hello_serial is built
+    test -f ${TARGET_DIR}/pico-examples/build/hello_world/serial/hello_serial.uf2 || fail
+
+    # test that hello_usb is built
+    test -f ${TARGET_DIR}/pico-examples/build/hello_world/usb/hello_usb.uf2 || fail
+}
+
+validate_pico_playground() {
+    validate_git_repo pico-playground
 }
 
 phase_2() {
@@ -274,13 +387,28 @@ phase_2() {
     done
 
     build_examples
+
+    validate_pico_examples
+    validate_pico_extras
+    validate_pico_playground
+}
+
+validate_picotool() {
+    validate_git_repo picotool
+
+    # test that the binary is built
+    test -x ${TARGET_DIR}/picotool/build/picotool || fail
+
+    # test that picotool is installed in the expected location
+    test -x /usr/local/bin/picotool || fail
 }
 
 setup_picotool() {
     # Downloads, builds, and installs picotool
     echo "Setting up picotool"
 
-    cd "${TARGET_DIR}"
+    # Save the working directory
+    pushd "${TARGET_DIR}" >> /dev/null
 
     clone_raspberrypi_repo picotool
     cd "${TARGET_DIR}/picotool"
@@ -290,14 +418,25 @@ setup_picotool() {
     make -j${JNUM}
 
     echo "Installing picotool to /usr/local/bin/picotool"
-    sudo cp picotool /usr/local/bin/
+    $(sudo_wrapper) cp "${TARGET_DIR}/picotool/build/picotool" /usr/local/bin/
+
+    # Restore the working directory
+    popd >> /dev/null
+}
+
+validate_openocd() {
+    validate_git_repo openocd
+
+    # test that the binary is built
+    test -x ${TARGET_DIR}/openocd/src/openocd || fail
 }
 
 setup_openocd() {
     # Download, build, and install OpenOCD for picoprobe and bit-banging without picoprobe
     echo "Setting up OpenOCD"
 
-    cd "${TARGET_DIR}"
+    # Save the working directory
+    pushd "${TARGET_DIR}" >> /dev/null
 
     clone_raspberrypi_repo openocd picoprobe --depth=1
     cd "${TARGET_DIR}/openocd"
@@ -309,14 +448,25 @@ setup_openocd() {
     fi
     ./configure ${OPTS}
     make -j${JNUM}
-    sudo make install
+    $(sudo_wrapper) make -C "${TARGET_DIR}/openocd" install
+
+    # Restore the working directory
+    popd >> /dev/null
+}
+
+validate_picoprobe() {
+    validate_git_repo picoprobe || fail
+
+    # test that the binary is built
+    test -f ${TARGET_DIR}/picoprobe/build/picoprobe.uf2 || fail
 }
 
 setup_picoprobe() {
     # Download and build picoprobe. Requires that OpenOCD is already setup
     echo "Setting up picoprobe"
 
-    cd "${TARGET_DIR}"
+    # Save the working directory
+    pushd "${TARGET_DIR}" >> /dev/null
 
     clone_raspberrypi_repo picoprobe
     cd "${TARGET_DIR}/picoprobe"
@@ -324,25 +474,36 @@ setup_picoprobe() {
     cd build
     cmake ..
     make -j${JNUM}
+
+    # Restore the working directory
+    popd >> /dev/null
+}
+
+validate_vscode_linux() {
+    dpkg-query -s code  >& /dev/null || fail
 }
 
 install_vscode_linux() {
     # Install Visual Studio Code
 
     # VS Code is specially added to Raspberry Pi OS repos, but might not be present on Debian/Ubuntu. So we check first.
-    if ! apt list code; then
+    if ! apt-cache show code >& /dev/null; then
         echo "It appears that your APT repos do not offer Visual Studio Code. Skipping."
         return
     fi
 
     echo "Installing Visual Studio Code"
 
-    sudo apt install -y code
+    $(sudo_wrapper) apt install -y code
 
     # Get extensions
     code --install-extension marus25.cortex-debug
     code --install-extension ms-vscode.cmake-tools
     code --install-extension ms-vscode.cpptools
+}
+
+validate_vscode_mac() {
+    echo "Not yet implemented: testing Visual Studio Code on macOS"
 }
 
 install_vscode_mac() {
@@ -354,15 +515,22 @@ phase_3() {
     echo "Setting up recommended tools"
 
     setup_picotool
+    validate_picotool
+
     setup_openocd
+    validate_openocd
+
     setup_picoprobe
+    validate_picoprobe
 
     # Install Visual Studio Code
     if mac; then
         install_vscode_mac
+        validate_vscode_mac
     else
-        if dpkg-query -s xserver-xorg >> /dev/null; then
+        if dpkg-query -s xserver-xorg >& /dev/null; then
             install_vscode_linux
+            validate_vscode_linux
         else
             echo "Not installing Visual Studio Code because it looks like XWindows is not installed."
         fi
--- test/README.md
+++ test/README.md
@@ -1,74 +1,53 @@
 # Testing pico-setup
 
-The tests validate that `pico_setup.sh` _seems_ to setup your system correctly. It can be fooled. It does not test that the SDK or tools themselves work correctly, just that the setup script did its thing.
+`pico_setup.sh` validates that it has the expected effect for your specific setup. However, the script needs to work on a variety of platforms, so we test a variety.
 
-To execute the tests, first run `pico_setup.sh` from wherever you want to install the pico dev environment:
+***Be warned: the testing script is destructive.*** It configures the system in all kinds of ways to see if the script can handle it, and makes little attempt to put things back where they were before. These tests are expected to be run on a single-use system. You start with a fresh image at the beginning, and discard it at the end.
 
-```shell
-./pico_setup.sh
-```
-
-Logout and back in, so that your shell will pick up its environment variables. Then run the test from the same location as you ran `pico_setup.sh`:
-
-```shell
-./test/test
-```
-
-## What to test
-
-### Platform support
-
-We can test platform support by getting the platforms and verifying the script on each. The general process is:
+The general testing process is:
 
 1. Start from a fresh OS image.
-1. Do any necessary setup.
 1.1. On Raspberry Pi OS, you'll need to start the SSH service if necessary: `sudo /sbin/service ssh start`
-1.1. On EC2, you'll need to set a password: `sudo -i passwd $USER`
-1. Push the contents of the pico-setup repo from your dev host: `scp -r ~/git/raspberrypi/pico-setup/ pi@${TESTING_HOST}:`
-1. SSH to the test host and execute the setup: `ssh pi@${TESTING_HOST} pico-setup/pico_setup.sh`
-1. Logout to pick up env variables and test the setup: `ssh pi@${TESTING_HOST} pico-setup/test/test`
+1. Push the contents of the pico-setup repo from your dev host: `scp -r ~/git/raspberrypi/pico-setup/ ${TEST_USER}@${TEST_HOST}:`
+1. SSH to the test host and execute the test suite: `ssh ${TEST_USER}@${TEST_HOST} "export WRECK_THIS_COMPUTER=PLEASE_I_DESERVE_IT && pico-setup/test/test_local.sh" |& tee ${TEST_HOST}.log`
+
+The `tee` part captures all output and dumps it to a log file on the local host so you can investigate anything that went wrong. Additionally, logs for each test should be on the testing host, at `/tmp/test_*`
+
+If the test completes successfully, it will say that it's complete and return 0. If it fails, it will return non-zero.
+
+## Preparations
 
 For all tests on Raspberry Pi computers, start with a pristine image written by Raspberry Pi Imager. The focus is on more recent hardware such as Raspberry Pi 4, but this installer's dependencies are more about software than hardware, so it should work with any version of the hardware.
+
+Raspberry Pi is an ARM platform, so testing Raspberry Pi hardware technically covers ARM tests. However, the Linux distros built for Raspberry Pi hardware tend to be customized, so testing the mainline build is nonetheless relevant.
 
 The tests on non-Raspberry Pi computers can be executed on EC2 instances. The recommended EC2 instance sizes cost pennies/hour, except for mac1. mac1 costs 1.083 USD/hour in us-west-2, and requires dedicated host with minimum charge of 24 hours. You can launch and terminate instances all day on that dedicated host without cost, other than the hour it takes to clean up a terminated instance and launch a new one.
 
 Notes on EC2:
 
-* For the Linux x86_64 tests, t3.micro with standard 8 GiB EBS volume are sufficient.
-* For the AArch64 tests, t4g.micro with standard 8 GiB EBS volume are sufficient.
+* For the Linux x86 tests, t3.micro with standard 8 GiB EBS volume are sufficient.
+* For the ARM tests, t4g.micro with standard 8 GiB EBS volume are sufficient.
 * For the Windows Subsystem for Linux tests, t3.large with standard 30 GiB EBS volume are sufficient.
 * For the official Ubuntu AMIs from Canonical, the default username is `ubuntu`.
 * For the official Debian AMIs from Debian, the default username is `admin`.
 * For the macOS and Windows AMIs from Amazon, the default username is `ec2-user`.
 
-#### Summary
+## Summary
 
-TODO: update this
-|                               | Raspberry Pi | x86_64 | AArch64 |
+* ✅ Means this version is tested and works on this platform.
+* ? Means this version hasn't been tested on this platform.
+* (blank) Means this platform isn't relevant.
+
+| | Raspberry Pi | x86 | ARM |
 |-|-|-|-|
-| Raspberry Pi OS (32-bit)      | ✅ | Not tested | Not tested |
-| Raspberry Pi OS (64-bit)      | ✅ | Not tested | Not tested |
-| Raspberry Pi OS Lite (32-bit) | ✅ | Not tested | Not tested |
-| Ubuntu Desktop (64-bit)       | ?  | ✅ [EC2](https://aws.amazon.com/marketplace/pp/B08LQMCZGC) | ✅ [EC2](https://aws.amazon.com/marketplace/pp/B08LQMCZGC) |
-| Ubuntu Server (32-bit)        | ✅ | ? | ? |
-| Debian 10 (Buster)            | ✅ | ? | ? |
-| macOS 11 Big Sur              | -  | ✅ | ✅ |
-| Ubuntu on WSL                 | -  | ✅ | - |
-| Debian on WSL                 | -  | ✅ | - |
+| Raspberry Pi OS  | ✅ | ? |  |
+| Ubuntu 20        | ? | ✅ | ✅ |
+| Debian 10        | ? | ✅ | ✅ |
+| macOS 11 Big Sur |   | ? | ? |
+| Ubuntu on WSL    |   | ? |   |
+| Debian on WSL    |   | ? |   |
 
-#### Links to EC2 for convenience
-
-TODO: update this
-
-|                               | x86_64 | AArch64 |
-|-|-|-|-|
-| Ubuntu Desktop (64-bit)       | [EC2](https://aws.amazon.com/marketplace/pp/B08LQMCZGC) | [EC2](https://aws.amazon.com/marketplace/pp/B08LQMCZGC) |
-| Ubuntu Server (32-bit)        |
-| Debian 10 (Buster)            | [EC2](https://aws.amazon.com/marketplace/pp/B0859NK4HC) | [EC2](https://aws.amazon.com/marketplace/pp/B0859NK4HC) |
-| macOS 11 Big Sur              |
-| Windows Subsustem for Linux   |
-
-#### Windows Subsystem for Linux
+## Windows Subsystem for Linux
 
 Install WSL according to https://docs.microsoft.com/en-us/windows/wsl/install-on-server.
 
@@ -88,11 +67,3 @@ wget https://raw.githubusercontent.com/raspberrypi/pico-setup/master/pico_setup.
 chmod +x pico_setup.sh
 ./pico_setup.sh
 ```
-
-## Scenarios
-
-Here are some scenarios worth testing:
-
-* Shells: bash, zsh. It should be enough to switch shells and rerun the installer and test scripts. You don't have to wipe the SD card/disk. Note that bash is default on Raspberry Pi OS and most other Linux distros. On macOS, the default is zsh.
-* .profile or .zprofile does/doesn't end in newline
-* PWD containing space. The installer script seems to handle it but an OpenOCD submodule, jimctl, doesn't. I think that jimctl is an alternative to picoprobe, so maybe we shouldn't try to build (or init) it at all. Or maybe we could do everyone a favor and send them a PR fixing it.
