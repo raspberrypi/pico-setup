1a9a53e126a30d113c1cf930514e7f442fd89ab7
will-v-pi <108662275+will-v-pi@users.noreply.github.com>
2025-06-04 11:27:17 +0100

    Fix picotool usage (#43)
    Install picotool before building SDK projects
    Fixes #41 and fixes #42

--- pico_setup.sh
+++ pico_setup.sh
@@ -75,6 +75,29 @@ cd $OUTDIR
 # Pick up new variables we just defined
 source ~/.bashrc
 
+# Debugprobe and picotool
+for REPO in picotool debugprobe
+do
+    DEST="$OUTDIR/$REPO"
+    REPO_URL="${GITHUB_PREFIX}${REPO}${GITHUB_SUFFIX}"
+    git clone $REPO_URL
+
+    # Build both
+    cd $DEST
+    git submodule update --init
+    mkdir build
+    cd build
+    cmake ../
+    make -j$JNUM
+
+    if [[ "$REPO" == "picotool" ]]; then
+        echo "Installing picotool"
+        sudo make install
+    fi
+
+    cd $OUTDIR
+done
+
 # Build blink and hello world for pico and pico2
 cd pico-examples
 for board in pico pico2
@@ -95,29 +118,6 @@ do
 done
 
 cd $OUTDIR
-
-# Debugprobe and picotool
-for REPO in debugprobe picotool
-do
-    DEST="$OUTDIR/$REPO"
-    REPO_URL="${GITHUB_PREFIX}${REPO}${GITHUB_SUFFIX}"
-    git clone $REPO_URL
-
-    # Build both
-    cd $DEST
-    git submodule update --init
-    mkdir build
-    cd build
-    cmake ../
-    make -j$JNUM
-
-    if [[ "$REPO" == "picotool" ]]; then
-        echo "Installing picotool to /usr/local/bin/picotool"
-        sudo cp picotool /usr/local/bin/
-    fi
-
-    cd $OUTDIR
-done
 
 if [ -d openocd ]; then
     echo "openocd already exists so skipping"
